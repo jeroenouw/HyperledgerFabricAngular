b7ce9c128bafe580d589e8322533d58e69a3f775
jeroenouw <jeroenouw@outlook.com>
2018-02-04 15:12:55 +0100

    Created home component and all modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,18 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app.routing.module';
+import { AppRoutingModule } from './app.routing';
+
+import { ParticipantsModule } from './participants/participants.module';
+import { AssetsModule } from './asset/assets.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { SharedModule } from './shared/shared.module';
 
 import { Configuration } from './configuration';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    ParticipantsModule,
+    AssetsModule,
+    TransactionsModule,
+    SharedModule
   ],
   providers: [
     Configuration
---
+++ client/src/app/transactions/transactions.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+  ],
+  providers: [
+  ],
+  exports: [
+  ]
+})
+export class TransactionsModule { }
